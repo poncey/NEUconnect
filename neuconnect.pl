727b7cb0c9366926dbfb518de2ffb66161f10d2b
Poncey <sky55wolf@live.com>
2018-12-17 14:31:05 +0800

    modified and simplify

--- neuconnect.pl
+++ neuconnect.pl
@@ -1,35 +1,6 @@
-use Net::SSL;
 use LWP::UserAgent;
-use Term::ReadKey;
 
-# subprogram for enter password
-sub enter_password {
-	my $password = "";
-	# Start reading the keys 
-    ReadMode(4); #Disable the control keys 
-    while(ord($key = ReadKey(0))!= 10) 
-    # This will continue until the Enter key is pressed (decimal value of 10) 
-    { 
-        # For all value of ord($key) see http://www.asciitable.com/
-        if(ord($key) == 127 || ord($key) == 8) { 
-            # DEL/Backspace was pressed 
-            #1. Remove the last char from the password 
-            chop($password); 
-            #2 move the cursor back by one, print a blank character, move the cursor back by one 
-            print"\b"; 
-            print"";
-        }elsif(ord($key) <32){ 
-            # Do nothing with these control characters 
-        }else{ 
-            $password = $password.$key; 
-            print"*"; 
-        } 
-    } 
-    ReadMode(0); #Reset the terminal once we are done 
-    return($password)
-}
-
-system(`export PERL_LWP_SSL_VERIFY_HOSTNAME=0`);
+system("export", "PERL_LWP_SSL_VERIFY_HOSTNAME=0");
 $action = 'login';
 $argsize = @ARGV;
 if($argsize == 1){
@@ -38,7 +9,7 @@ if($argsize == 1){
 	print("Insert the user name:\n");
 	$name = <STDIN>;
 	print("Insert the password:\n");
-	$passwd = enter_password();
+	$passwd = <STDIN>;
 	
 }
 elsif($argsize == 3){
